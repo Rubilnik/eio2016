3e0ce14e21ae2427191d20890b9ddb4b7e56b7cd
Mihkel Auväärt <mihkel.auvaart@gmail.com>
2016-10-19 22:17:00 +0300

    Added partial factorial project

---
+++ faktoriaal/faktoriaal/fact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoriaal
+{
+    class fact
+    {
+
+        private static int[] readInput(string fileName)
+        {
+            var file = new System.IO.StreamReader(fileName);
+            var text = file.ReadLine().Split(' ');
+            file.Close();
+
+            var numbers = new int[2];
+            for (var i = 0; i < text.Length; i++)
+            {
+                numbers[i] = int.Parse(text[i]);
+            }
+            
+            return numbers;
+        }
+
+        private static int[] fillInputGap(int[] numbers)
+        {
+            var delta = numbers[1] - numbers[0];
+            var filledNumbers = new int[delta + 1];
+            filledNumbers[0] = numbers[0];
+            for (var i = 1; i <= delta; i++)
+            {
+                filledNumbers[i] = filledNumbers[0] + i;
+            }
+            return filledNumbers;
+        }
+
+        static void Main(string[] args)
+        {
+            var numbers = fillInputGap(readInput("factsis.txt"));
+        }
+    }
+}
